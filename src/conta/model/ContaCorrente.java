270b9e99bf017126724ac35dcc544ef71af2673c
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-20 17:49:42 -0300

    Add limit to ContaCorrente class

--- src/conta/model/ContaCorrente.java
+++ src/conta/model/ContaCorrente.java
@@ -1,6 +1,30 @@
 package conta.model;
 
 public class ContaCorrente extends Conta {
- 
+  private int limite;
+
+  public ContaCorrente(int numero, int agencia, String tipo, String titular, float saldo, int limite) {
+    super(numero, agencia, agencia, titular, saldo);
+    this.limite = limite;
+  }
+
+  public int getLimite() {
+    return this.limite;
+  }
+
+  public void setLimite(int limite) {
+    this.limite = limite;
+  }
+
+  @Override
+  public boolean sacar(float valor) {
+      if(this.getSaldo() + this.getLimite() < valor){
+        System.out.println("\nSaldo insuficiente");
+        return false;
+      }
+    this.setSaldo(this.getSaldo() - valor);
+    System.out.println("\nSaque efetuado");
+      return true;
+  }
 
 }
