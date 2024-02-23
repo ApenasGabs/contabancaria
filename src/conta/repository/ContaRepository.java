0e1b8186ccf08746539f8efdab1a28cb9804c79f
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-23 15:23:36 -0300

    Refactor ContaRepository interface to include additional banking methods and CRUD operations

--- src/conta/repository/ContaRepository.java
+++ src/conta/repository/ContaRepository.java
@@ -1,8 +1,28 @@
 package conta.repository;
 
+import conta.model.Conta;
+
 public interface ContaRepository {
-	public void depositar(float valor);
-	public void  visualizar();
-	public boolean sacar(float valor);
+	/**
+	 * Métodos Bancários
+	 */
+	public void sacar(int numero, float valor);
+
+	public void depositar(int numero, float valor);
+
+	public void transferir(int numeroOrigem, int numeroDestino, float valor);
+
+	/**
+	 * CRUD da Conta
+	 */
+	public void procurarPorNumero(int numero);
+
+	public void listarTodas();
+
+	public void cadastrar(Conta conta);
+
+	public void atualizar(Conta conta);
+
+	public void deletar(int numero);
 
 }
