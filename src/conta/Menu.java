d30de0579045a5b732a45e61dcf82b6d15213cac
Gabriel Rodrigues <gabers357@gmail.com>
2024-02-26 07:28:57 -0300

    Add menu functionality to the main class

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,27 +1,73 @@
 package conta;
 
+import java.util.Scanner;
+
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 
 public class Menu {
-
 	public static void main(String[] args) {
-		ContaPoupanca conta1 = new ContaPoupanca(1, 555, 1, "Gabriel", 321);
-		ContaCorrente conta2 = new ContaCorrente(1, 555, "1", "Gabriel", 321, 500);
-		conta1.setAgencia(999);
-		conta2.setAgencia(666);
+		boolean condicaoQueAindaNaoPenseiEmComoFazer = true;
+		do {
+			exibirMenuPrincipal();
+			String opcao = lerOpcaoUsuario();
 
-		if (conta1.sacar(100))
-			System.out.println("Saque efetuado com sucesso. O novo saldo é de: " + conta1.getSaldo());
-		else
-			System.out.println("O saldo é insuficiente!");
-
-		conta1.visualizar();
-		conta1.depositar(55);
-		conta1.visualizar();
-		conta2.visualizar();
-		conta2.depositar(25);
-		conta2.visualizar();
+			switch (opcao) {
+				case "1" -> criarConta();
+				case "2" -> listarTodasAsContas();
+				case "3" -> buscarContaPorNumero();
+				case "4" -> atualizarDadosConta();
+				case "5" -> apagarConta();
+				case "6" -> realizarSaque();
+				case "7" -> realizarDeposito();
+				case "8" -> realizarTransferencia();
+				case "9" -> sairDoPrograma();
+				default -> mostrarOpcaoInvalida();
+			}
+		}while (condicaoQueAindaNaoPenseiEmComoFazer);
 	}
 
+	private static String lerOpcaoUsuario() {
+		return "";
+	}
+
+	private static void criarConta() {
+
+	}
+
+	private static void listarTodasAsContas() {
+
+	}
+
+	private static void buscarContaPorNumero() {
+
+	}
+
+	private static void atualizarDadosConta() {
+
+	}
+
+	private static void apagarConta() {
+
+	}
+
+	private static void realizarSaque() {
+
+	}
+
+	private static void realizarDeposito() {
+
+	}
+
+	private static void realizarTransferencia() {
+	}
+
+	private static void mostrarOpcaoInvalida() {
+	}
+
+	private static void exibirMenuPrincipal() {
+	}
+
+	private static void sairDoPrograma() {
+	}
 }
